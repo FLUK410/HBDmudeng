612281cadcd840050774006d8bc9773f701dca6f
Felix <159446532+FLUK410@users.noreply.github.com>
2025-11-08 19:00:28 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openLetter() {
-  document.getElementById("envelope").style.display = "none";
-  document.getElementById("letterContent").style.display = "block";
-}
-
-function showMore() {
-  document.getElementById("moreContent").style.display = "block";
-}
